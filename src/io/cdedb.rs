521ef87822f6fc4117593032f5a344a5857970d8
Michael Thies <mail@mhthies.de>
2024-04-07 11:41:49 +0200

    Bump CdEDB export version

--- src/io/cdedb.rs
+++ src/io/cdedb.rs
@@ -24,8 +24,8 @@ use std::cmp::max;
 use log::{info, warn};
 
 const MINIMUM_EXPORT_VERSION: (u64, u64) = (7, 0);
-const MAXIMUM_EXPORT_VERSION: (u64, u64) = (16, std::u64::MAX);
-const OUTPUT_EXPORT_VERSION: (u64, u64) = (16, 0);
+const MAXIMUM_EXPORT_VERSION: (u64, u64) = (17, std::u64::MAX);
+const OUTPUT_EXPORT_VERSION: (u64, u64) = (17, 1);
 
 pub struct ImportAmbienceData {
     event_id: u64,
