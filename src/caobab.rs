32846e9df2ff2ac970210bf51c3a5dd2061e4d8b
Michael Thies <mail@mhthies.de>
2026-06-28 14:06:27 +0200

    Add missing copyright headers and update copyright years

--- src/caobab.rs
+++ src/caobab.rs
@@ -1,4 +1,4 @@
-// Copyright 2020 by Michael Thies <mail@mhthies.de>, Gabriel Guckenbiehl <gabriel.guckenbiehl@gmx.de>
+// Copyright 2020-2024 by Michael Thies <mail@mhthies.de>, Gabriel Guckenbiehl <gabriel.guckenbiehl@gmx.de>
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
 // the License. You may obtain a copy of the License at
